0cdbe9b5d63f4e727da5548e524a35c587d59371
Andrew Kuzmenko <abejib93@gmail.com>
2021-12-01 16:03:25 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+#Создаём каталоги под файлы, перед этим удаляем существующие
+shutil.rmtree("instruments")
+shutil.rmtree("sheets")
+shutil.rmtree("charts")
+os.mkdir("instruments")
+os.mkdir("sheets")
+os.mkdir("charts")
+instruments = []
+
+#Создаем массив с кодами инструмента
+with open("/Users/admin/Onlinx/file.txt", "r") as ins:
+    array = []
+    for line in ins:
+        array.append(line)
+
+#Создаем конфиги инструментов
+os.chdir("instruments")
+for i in range(len(array)):
+    x = str(array[i])
+    f = open(x[:-1] + ".cfg", "w", encoding='utf-8')
+    f.write("description=")
+    f.close()
+#Создаем строку для добавления в файл площадки
+instr = str(array)
+os.chdir("..")
+os.chdir("sheets")
+s = open("instruments.txt", "w", encoding='utf-8')
+s.write(instr)
+s.close()
+
+#Создаем конфиги с параметрами для графика
+os.chdir("..")
+os.chdir("charts")
+for i in range(len(array)):
+    x = str(array[i])
+    d = open("MMVB." + x[:-1] + ".cfg", "w", encoding='utf-8')
+    d.write("fields=Last VDay,R VDay,Bid,Ask\n")
+    d.write("periods=1,5,10,15,30,60,1440,W,M,Y\n")
+    d.write("mask=0000.00\n")
+    d.write("filter=0.01\n")
+    d.write("vmask=0000000000")
+    d.close()
+
+
