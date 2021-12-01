a734b0886a400987a57435880a9af4efdbff4dbb
Andrew Kuzmenko <abejib93@gmail.com>
2021-12-01 16:22:08 +0300

    Ручной ввод площадки

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import shutil
-#Создаём каталоги под файлы, перед этим удаляем существующие
+
+# Создаём каталоги под файлы, перед этим удаляем существующие
 shutil.rmtree("instruments")
 shutil.rmtree("sheets")
 shutil.rmtree("charts")
@@ -8,40 +9,41 @@ os.mkdir("instruments")
 os.mkdir("sheets")
 os.mkdir("charts")
 instruments = []
+# Указываем фанансовую площадку
+platform = str(input())
 
-#Создаем массив с кодами инструмента
+# Создаем массив с кодами инструмента
 with open("/Users/admin/Onlinx/file.txt", "r") as ins:
     array = []
     for line in ins:
         array.append(line)
 
-#Создаем конфиги инструментов
+# Создаем конфиги инструментов
 os.chdir("instruments")
 for i in range(len(array)):
     x = str(array[i])
     f = open(x[:-1] + ".cfg", "w", encoding='utf-8')
     f.write("description=")
     f.close()
-#Создаем строку для добавления в файл площадки
+# Создаем строку для добавления в файл площадки
 instr = str(array)
-instr = instr.replace("'","").replace("[","").replace(r"\n","").replace("]","").replace(" ","")
+instr = instr.replace("'", "").replace("[", "").replace(r"\n", "").replace("]", "").replace(" ", "")
 os.chdir("..")
 os.chdir("sheets")
 s = open("instruments.txt", "w", encoding='utf-8')
 s.write(instr)
 s.close()
 
-#Создаем конфиги с параметрами для графика
+# Создаем конфиги с параметрами для графика
 os.chdir("..")
 os.chdir("charts")
 for i in range(len(array)):
     x = str(array[i])
-    d = open("MMVB." + x[:-1] + ".cfg", "w", encoding='utf-8')
+    d = open(platform + "." + x[:-1] + ".cfg", "w", encoding='utf-8')
+#Ниже прописываем маску для графиков
     d.write("fields=Last VDay,R VDay,Bid,Ask\n")
     d.write("periods=1,5,10,15,30,60,1440,W,M,Y\n")
     d.write("mask=0000.00\n")
     d.write("filter=0.01\n")
     d.write("vmask=0000000000")
     d.close()
-
-
